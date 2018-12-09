fc9331c109dcd57e3c96b717a187f9c32ffe79f4
jmk5420 <jmk5420@rit.edu>
2018-12-09 18:40:42 -0500

    New page and test

--- projects/project3/js/int.js
+++ projects/project3/js/int.js
@@ -1,5 +1,23 @@
 'use strict';
 
-$(".menutog").click(function () {
-    $(".menuitem").slideToggle("fast");
+//hamburger menu
+let ninja = $(".menuitem").hide(); // Makes a varable to hide the menu
+//Makes a pull down menu for the hamburger menu
+$(".menutog").click(function () { // selects the target that holds the menu
+    $(".submenu").val(ninja); // set hide as default
+    $(".menuitem").slideToggle("fast"); // Tell the page what pulls down from the menu
+});
+
+//$(document).ready(function () {
+//    $("button").click(function () {
+//        $(".merch").toggle();
+//    });
+//});
+
+//mechendice hiding
+let nope = $(".merch").hide(); // Makes a varable to hide the merch
+//Makes the merch apper
+$("button").click(function () { // selects the target that holds the merch
+    $(".submenu").val(nope); // set hide as default
+    $(".merch").slideToggle("fast"); // Tell the page what pulls down from the button
 });
